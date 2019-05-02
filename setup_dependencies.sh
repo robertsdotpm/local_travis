eacb1957e7d30876153d56606226f9afc2fee170
Matthew Roberts <matthew@roberts.pm>
2019-05-02 23:11:55 +1000

    x

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,22 +1,18 @@
-rvm install 2.3.0
-rvm use 2.3.0
+export PATH="$PATH:/home/travis/.rvm/bin"
 
+rvm install 2.5.3
+rvm use 2.5.3
 
 cd /home/travis/builds
 git clone https://github.com/travis-ci/travis-build.git
 cd travis-build
-gem install travis
-yes Y | travis version
-ln -s `pwd` /home/travis/.travis/travis-build
+mkdir -p ~/.travis
+ln -s $PWD ~/.travis/travis-build
+gem install bundler
+bundle install --gemfile ~/.travis/travis-build/Gemfile
+bundler binstubs travis
 
-gem install bundler:1.16.6
-gem install rake -v 12.3.1
 yes Y | travis version
-ln -s `pwd` /home/travis/.travis/travis-build
-export PATH="$PATH:/home/travis/.rvm/bin"
-bundle install
 
-# Move rune stones again.
-gem install rake -v 12.3.1
-chmod +x /home/travis/.travis/travis.sh
-/home/travis/.travis/travis.sh
+
+
