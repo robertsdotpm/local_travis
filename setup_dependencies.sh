334b61d01c5dc7c7bccc18f0863de88b5fc12262
Matthew Roberts <matthew@roberts.pm>
2019-05-02 18:57:26 +1000

    Try fix bundle errors.

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,14 +1,16 @@
 rvm install 2.4.0
 rvm use 2.4.0
+gem update --system 
+bundle update --bundler
+
 cd /home/travis/builds
 git clone https://github.com/travis-ci/travis-build.git
 cd travis-build
 gem install travis
 yes Y | travis version
 ln -s `pwd` ~/.travis/travis-build
+
 bundle install
-gem update --system 
 bundler add travis
-gem update --system 
 bundler binstubs travis
-gem update --system
+
