4ba9892055ab13beec82b2ebccfb43c81e68acf4
Matthew Roberts <matthew@roberts.pm>
2019-05-02 23:17:43 +1000

    x

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -2,13 +2,14 @@ export PATH="$PATH:/home/travis/.rvm/bin"
 
 rvm install 2.5.3
 rvm use 2.5.3
+gem i bundler -v 1.17.3
 
 cd /home/travis/builds
 git clone https://github.com/travis-ci/travis-build.git
 cd travis-build
 mkdir -p ~/.travis
 ln -s $PWD ~/.travis/travis-build
-gem install bundler
+
 bundle install --gemfile ~/.travis/travis-build/Gemfile
 bundler binstubs travis
 
